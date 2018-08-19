79b9d53433843d975791b9ec1d05891da8b4f8b9
Steve Onyeneke <s.c.onyeneke.09@aberdeen.ac.uk>
2018-08-19 05:20:10 +0100

    Added modal | Refactored app

--- armstrong.js
+++ armstrong.js
@@ -7,54 +7,84 @@
 
 //DOM MANIPULATION
 //1. Should inject initial HTML
-//2. Should display an input field 
+//2. Should display an input field
 //3. Should display a "run check" button
 //4. Should deactivate "run check" button on invalid input
 //5  Should display massege if deactivated "run check" button is clicked
 //6. Should display result on a div
 /***************************************************/
 
-let btn = document.getElementById("checkBtn");
+const btn = document.getElementById("checkBtn");
+const display = document.getElementById("checkResult");
+const inputBox = document.getElementById("input");
 
-//armstrong() interacts with the DOM to display results computed by checkValue() to the user
-function armstrong(){ 
-  const input = document.getElementById("input").value;
-  const display = document.getElementById("checkResult")
-  if (input.length === 0){
-    display.innerHTML= `<span style="color: red; text-shadow:none;">Please enter a 3-digit number`;
-    return
-  }
-  if (input.length !== 3){
-    display.innerHTML= `<span style="color: red; text-shadow:none;">Sorry! Only 3 digit numbers are accepted`;
-    return
-  }
+//Interacts with the DOM to display results computed by checkValue() to the user and displays related info if required conditions (eg. no input, incorrect length) are not met.
+btn.onclick = () => {
+  const input = inputBox.value;
   const result = checkValue(input);
-  if (result){
-    display.innerHTML = `<span style="color: greenyellow">Yay!!! ${input} is an Armstrong number!</span>`
-  }else{
-    display.innerHTML = `<span style="color: red">SORRY!...</span> ${input} is not an Armstrong number.`
+
+  if (input.length === 0) {
+    display.innerHTML = `<span style="color: red; text-shadow:none;">Please enter a 3-digit number`;
+    return;
+  }
+  if (input.length !== 3) {
+    display.innerHTML = `<span style="color: red; text-shadow:none;">Sorry! Only 3 digit numbers are accepted`;
+    return;
+  }
+  if (result) {
+    display.innerHTML = `<span style="color: greenyellow">Yay!!! ${input} is an Armstrong number!</span>`;
+  } else {
+    display.innerHTML = `<span style="color: red">SORRY!...</span> ${input} is not an Armstrong number.`;
+  }
+};
+
+//Performs actual calculations to verify the 'armstrong-ness' of the input and outputs result to the user
+function checkValue(val) {
+  let output = 0;
+  for (const char of val) {
+    output += Math.pow(char, 3);
+  }
+  if (output == val) {
+    return true || false;
   }
 }
 
-//checkInput verifies that the correct length of 3-digit integers is inputted by user and informs user if otherwise
-function checkInput(){
-  const input = document.getElementById("input").value;
-  if (input.length !== 3){
+//Styles the Run Check btn based on user input
+btn.onmouseover = () => {
+  const input = inputBox.value;
+  if (input.length !== 3) {
     btn.style.cursor = "not-allowed";
-    btn.style.opacity = 0.6;
-  }else{
+    btn.style.opacity = 0.3;
+    btn.style.backgroundColor = "red";
+  } else {
     btn.style.cursor = "pointer";
     btn.style.opacity = 1;
+    btn.style.backgroundColor = "green";
   }
-}
+};
 
-//checkValue performs actual calculations to verify the armstrong-ness of the input and outputs result to the user
-function checkValue(val){
-  let output = 0;
-  for(char of val){
-    output += char**3
+//Resets div display
+inputBox.onclick = () => {
+  display.innerHTML = "";
+};
+
+//***********MODAL**********/
+const modal = document.getElementById("modalDiv"); // Get the modal
+const mD = document.getElementById("info"); // Get the text that opens the modal
+const span = document.getElementsByClassName("close")[0]; // Get the <span> element that closes the modal
+
+// Open the modal when the user clicks on the text,
+mD.onclick = () => {
+  modal.style.display = "block";
+};
+// Close the modal when the user clicks on <span> (x)
+span.onclick = () => {
+  modal.style.display = "none";
+};
+
+// Also close the modal when the user clicks anywhere outside of the modal,
+window.onclick = event => {
+  if (event.target == modal) {
+    modal.style.display = "none";
   }
-  if(output==val){
-    return true || false
-  }
-}
+};
